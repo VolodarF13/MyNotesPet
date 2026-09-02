4978fc9f0b656e948ea8d7b86d53e34cbc31a452
VolodarF13 <volodiabodnarchyk2000@gmail.com>
2026-09-02 15:07:45 +0300

    Add User and Notes repositories

---
+++ src/main/java/goIt/ua/MyNotesPet/notes/NoteRepository.java
@@ -0,0 +1,8 @@
+package goIt.ua.MyNotesPet.notes;
+
+import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NoteRepository extends JpaRepositoryImplementation<Note, Long> {
+}
