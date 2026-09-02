00bd2db80506e8533baddf267b0d927f25f49424
VolodarF13 <volodiabodnarchyk2000@gmail.com>
2026-09-02 14:56:10 +0300

    Add User and Note entities

---
+++ src/main/java/goIt/ua/MyNotesPet/notes/Note.java
@@ -0,0 +1,37 @@
+package goIt.ua.MyNotesPet.notes;
+
+import goIt.ua.MyNotesPet.users.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "notes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "title", nullable = false, length = 255)
+    private String title;
+    @Column(name = "content", nullable = false, length = 255)
+    private String content;
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
---
+++ src/main/java/goIt/ua/MyNotesPet/users/User.java
@@ -0,0 +1,43 @@
+package goIt.ua.MyNotesPet.users;
+
+import goIt.ua.MyNotesPet.notes.Note;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "username", nullable = false, unique = true, length = 50)
+    private String username;
+    @Column(name = "email", nullable = false, unique = true, length = 254)
+    private String email;
+    @Column(name = "password", nullable = false, length = 50)
+    private String password;
+    @Column(name = "registrationDate", nullable = false)
+    private LocalDateTime registrationDate;
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private List<Note> notes = new ArrayList<>();
+}
