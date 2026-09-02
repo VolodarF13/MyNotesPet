72d5013795df2a248840e807192884fd39ba9d4d
VolodarF13 <volodiabodnarchyk2000@gmail.com>
2026-09-02 14:25:37 +0300

    Init commit...

---
+++ src/main/java/goIt/ua/MyNotesPet/MyNotesPetApplication.java
@@ -0,0 +1,13 @@
+package goIt.ua.MyNotesPet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyNotesPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyNotesPetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/goIt/ua/MyNotesPet/MyNotesPetApplicationTests.java
@@ -0,0 +1,13 @@
+package goIt.ua.MyNotesPet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyNotesPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
