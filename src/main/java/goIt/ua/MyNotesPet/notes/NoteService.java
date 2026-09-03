c1e4113a6585db67ad1c967e64ac8f1d94d362b8
VolodarF13 <volodiabodnarchyk2000@gmail.com>
2026-09-03 14:39:19 +0300

    add some anotation in NoteService

--- src/main/java/goIt/ua/MyNotesPet/notes/NoteService.java
+++ src/main/java/goIt/ua/MyNotesPet/notes/NoteService.java
@@ -1,4 +1,9 @@
 package goIt.ua.MyNotesPet.notes;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class NoteService {
 }
