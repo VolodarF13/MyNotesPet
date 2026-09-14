10bf732998fdbaaa56c85492450d962990bff403
VolodarF13 <volodiabodnarchyk2000@gmail.com>
2026-09-14 14:31:29 +0300

    Add validation for dto

--- src/main/java/goit/ua/mynotespet/notes/dto/request/UpdateNoteRequest.java
+++ src/main/java/goit/ua/mynotespet/notes/dto/request/UpdateNoteRequest.java
@@ -1,5 +1,6 @@
 package goit.ua.mynotespet.notes.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UpdateNoteRequest {
+    @NotBlank(message = "Title is required")
     @Size(min = 3, max = 250, message = "Title must be between 3 and 250 characters")
     private String title;
     @Size(max = 10000, message = "Content cannot exceed 10000 characters")
