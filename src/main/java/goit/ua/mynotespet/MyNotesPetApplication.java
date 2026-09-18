124f2cf072fcacb06335148d7da923d49d0affce
VolodarF13 <volodiabodnarchyk2000@gmail.com>
2026-09-18 15:03:10 +0300

    add CI/CD pipeline via GitHub Actions

--- src/main/java/goit/ua/mynotespet/MyNotesPetApplication.java
+++ src/main/java/goit/ua/mynotespet/MyNotesPetApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication()
 public class MyNotesPetApplication {
 
 	public static void main(String[] args) {
